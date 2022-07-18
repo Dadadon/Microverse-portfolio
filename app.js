04ec43a62750126bad07ce4deddb3f47efdceafe
Dadadon <davianberoni@yahoo.com>
2022-07-18 07:24:20 -0400

    fix linter errors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-  document.body.style.overflow = "hidden";
+  document.getElementById('myNav').style.width = '100%';
+  document.body.style.overflow = 'hidden';
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
-  document.body.style.overflow = "visible";
+  document.getElementById('myNav').style.width = '0%';
+  document.body.style.overflow = 'visible';
 }
 
 openNav();
