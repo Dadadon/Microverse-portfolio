ae8a45b3be38430b55abc46bbfc9d4ba34cafc45
Dadadon <davianberoni@yahoo.com>
2022-07-21 06:27:16 -0400

    fix linter errors

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ const nameInput = form.fullname;
 const formElts = form.querySelectorAll('input, textarea');
 
 const addToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
-const retrieveFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
+//const retrieveFromLocalStorage = (key) => JSON.parse(localStorage.getItem(key));
 
 formElts.forEach((fe) => {
   fe.addEventListener('input', () => {
